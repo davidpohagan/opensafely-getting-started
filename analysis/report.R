791f5c84b16ee392d358a4553cc2e028580e621f
David O'Hagan <davidoh@liverpool.ac.uk>
2025-03-10 14:34:38 +0000

    updates

--- analysis/report.R
+++ analysis/report.R
@@ -1,4 +1,5 @@
-library('tidyverse')
+library('ggplot2')
+library('readr')
 
 df_input <- read_csv(
   here::here("output", "dataset.csv.gz"),
@@ -6,8 +7,13 @@ df_input <- read_csv(
 )
 
 plot_age <- ggplot(data=df_input, aes(df_input$age)) + geom_histogram()
-
+plot_sex <- ggplot(data=df_input, aes(df_input$sex)) + geom_histogram(stat = "count")
 ggsave(
   plot= plot_age,
   filename="report.png", path=here::here("output"),
 )
+ggsave(
+  plot= plot_sex,
+  filename="reports.png", path=here::here("output"),
+)
+
